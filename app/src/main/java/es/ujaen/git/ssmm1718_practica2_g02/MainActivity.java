cc034bf80836adeefda0cc2aef66b1fb1e18bec5
juandy <jdgb0002@red.ujaen.es>
2017-10-18 12:23:01 +0200

    Subida inicial. - Inicio de la nueva interfaz grafica de Login.

---
+++ app/src/main/java/es/ujaen/git/ssmm1718_practica2_g02/MainActivity.java
@@ -0,0 +1,13 @@
+package es.ujaen.git.ssmm1718_practica2_g02;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
