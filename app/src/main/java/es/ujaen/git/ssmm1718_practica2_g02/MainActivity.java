decf3f0e79efc644cf278f217da9726763d5f7ce
juandy <jdgb0002@red.ujaen.es>
2017-10-25 12:24:15 +0200

    Sesion 2: -Agregado Aparicion de Opcion de Servidor Falta: -Acabar interfaz _Hacer autentificación

--- app/src/main/java/es/ujaen/git/ssmm1718_practica2_g02/MainActivity.java
+++ app/src/main/java/es/ujaen/git/ssmm1718_practica2_g02/MainActivity.java
@@ -1,13 +1,32 @@
 package es.ujaen.git.ssmm1718_practica2_g02;
 
+import android.net.Uri;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MoreOptionFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+
+    public void activarOpciones(View view){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        MoreOptionFragment fragment = new MoreOptionFragment();
+        fragmentTransaction.add(R.id.more_option_layout,fragment);
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
